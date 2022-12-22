de17951a0d82983325a889cd5bdf5e2cfb7dab38
Res1dence <res1dence@outlook.com>
2022-12-22 10:23:06 +0100

    quicksort finished

--- Quellcodes/Aufg1.py
+++ Quellcodes/Aufg1.py
@@ -16,6 +16,20 @@ def selectionSort(array):
         array[i], array[min_idx] = array[min_idx], array[i]
     return array
 
-array = [7,5,8,1]
-array=selectionSort(array)
-print(array)
+def quickSort(array):
+    if len(array) <= 1:
+        # Start of Recursion: singleton array is sorted
+        return array
+    pivot = array[0]
+    # Take the first number of the array as the pivot value
+    llist, mlist, rlist = [], [], []
+    # Define empty lists to store elements greater than/less than/equal to the base value
+    for i in range(0, len(array)):
+        # Traverse the array and categorize the elements into three parts
+        if array[i] < pivot:
+            llist.append(array[i])
+        elif array[i] > pivot:
+            rlist.append(array[i])
+        else:
+            mlist.append(array[i])
+    return quickSort(llist) + mlist + quickSort(rlist) # Recursive call: put all sorted parts together
